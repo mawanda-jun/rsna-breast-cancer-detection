6dcc0c2fdb3c483f33415ae2bf7ba8b157281bf1
mawanda-jun <giovanni.cavallin@outlook.com>
2022-12-12 09:21:23 +0100

    added new configurations

--- train_batch.sh
+++ train_batch.sh
@@ -1,9 +1,26 @@
 cd src/
 
-# python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/exp1.yaml
-# python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/exp2.yaml
-# python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/exp3.yaml
-# python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/exp4.yaml
-# python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/exp5.yaml
-# python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_baselinepos.yaml
-python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_allaug_cyclic.yaml
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_brightness.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_randomresizedcrop.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_tone.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_clahe.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_coarsedropout.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_pixdrop.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_huesat.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_gaussblur.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_elastic.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_gamma.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_iso.yaml
+sleep 30
+python trainer.py --path /projects/rsna-breast-cancer-detection/src/configs/eff4_512_sim_smooth_neg1_perspective.yaml
+sleep 30
