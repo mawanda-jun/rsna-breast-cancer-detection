0d3095427db83c3a07dd1bc677a85ae2b3b76e04
mawanda-jun <giovanni.cavallin@outlook.com>
2022-12-21 16:22:30 +0100

    added support for 16-bit depth

--- dataset_utils/preprocess.py
+++ dataset_utils/preprocess.py
@@ -2,30 +2,43 @@ import os
 from pathlib import Path
 import pydicom
 from pydicom.pixel_data_handlers.util import apply_voi_lut
+# import dicomsdl
 import numpy as np
 from PIL import Image
-import gzip
 import cv2
 import concurrent.futures
+from timeit import default_timer as timer
 from tqdm import tqdm
 
+depth_to_dtype = {
+    8: np.uint8,
+    16: np.uint16
+}
+
+# def new_read_xray(path):
+#     dicom = dicomsdl.open(str(path))
+#     bits = dicom.BitsStored
+#     data = dicom.pixelData()
+
+#     if dicom.getPixelDataInfo()['PhotometricInterpretation'] == "MONOCHROME1":
+#         data = (2**bits - 1) - data
+    
+#     return data, bits
 
 def read_xray(path, voi_lut = True, fix_monochrome = True):
     dicom = pydicom.read_file(path)
     bits = dicom.BitsStored
-    if dicom.get('VOILUTSequence'):
-        print(f"{path} has VOILUT sequence!")
 
     # VOI LUT (if available by DICOM device) is used to transform raw DICOM data to "human-friendly" view
     if voi_lut:
         data = apply_voi_lut(dicom.pixel_array, dicom)
     else:
         data = dicom.pixel_array
-               
+
     # depending on this value, X-ray may look inverted - fix that:
     if fix_monochrome and dicom.PhotometricInterpretation == "MONOCHROME1":
         data = (2**bits - 1) - data
-        
+       
     return data, bits
 
 
@@ -42,33 +55,21 @@ def crop_roi_from_image(img: np.ndarray):
     
     return x, y, w, h
 
-def convert_fun(img_path: Path, dest_path: Path):
+def convert_fun(img_path: Path, dest_path: Path, depth: int=8):
     # Create directory
     dest_path.parent.mkdir(parents=True, exist_ok=True)
     # Actual reading
+    # img, bits = new_read_xray(img_path)
+    # img, bits = read_xray(img_path, voi_lut=True)
     img, bits = read_xray(img_path, voi_lut=True)
     # Convert to uint8 to cut out interesting portion of image (not working in uint16)
-    img_8 = ((img / (2**bits - 1)) * (2**8 - 1)).astype(np.uint8)
+    img_8 = (img * 2**(8 - bits)).astype(np.uint8)
     x, y, w, h = crop_roi_from_image(img_8)
-    # Crop original image
-    cropped_img = img[y:y+h, x:x+w, ...]
-    # Correct color space so that normalization works as it should.
-    cropped_img = (cropped_img * 2**(16 - bits)).astype(np.uint16)
-    # # Resize to contain dimension
-    # height, width = cropped_img.shape
-    # max_dim = 4096
-    # if max([height, width]) > max_dim:
-    #     if height > width: 
-    #         ratio = max_dim / height
-    #     elif width >= height:
-    #         ratio = max_dim / width
-    #     new_height = int(round(height*ratio))
-    #     new_width = int(round(width*ratio))
-    #     try:
-    #         cropped_img = cv2.resize(cropped_img, dsize=(new_width, new_height))
-    #     except Exception:
-    #         print(f"It was not possible to resize {str(img_path)}, skipping...")
-    #         return
+    cropped_img = img_8[y:y+h, x:x+w, ...]
+
+    # # Working with multiple depths
+    # cropped_img = img[y:y+h, x:x+w, ...]
+    # cropped_img = (cropped_img * 2**(depth - bits)).astype(depth_to_dtype[depth])
 
     # Save cropped image
     try:
@@ -88,20 +89,28 @@ def convert_dataset(dataset_dir: Path, output_dir: Path):
         output_dir (_type_): _description_
     """
     input_paths = list(dataset_dir.glob("**/*.dcm"))
-    output_paths = [output_dir / img_path.relative_to(img_path.parent.parent).with_suffix('').with_suffix('.tif') for img_path in input_paths]
+    output_paths = [output_dir / img_path.relative_to(img_path.parent.parent).with_suffix('').with_suffix('.png') for img_path in input_paths]
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as worker:
-        print(list(tqdm(worker.map(
-            convert_fun,
-            input_paths,
-            output_paths
-        ), total=len(input_paths))))
+    # with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as worker:
+    #     print(list(tqdm(worker.map(
+    #         convert_fun,
+    #         input_paths,
+    #         output_paths
+    #     ), total=len(input_paths))))
+    i = 0
+    start = timer()
+    progressbar = tqdm(zip(input_paths, output_paths))
+    for input_path, output_path in progressbar:
+        convert_fun(input_path, output_path)
+        i += 1
+        progressbar.set_description(f"imgs/s: {i / (timer()-start):.4f}")
+
     
 
 if "__main__" in __name__:
     # img_path = Path("/data/rsna-breast-cancer-detection/train_images/5325/324732883.dcm")
     # Image.fromarray(read_xray(img_path)).save("img.png")
     # Image.fromarray(crop_roi_from_image(read_xray(img_path))).save("roi_only.png")
-    dataset_path = Path("/original_dataset/train_images/")
-    output_path = Path("/data/rsna-breast-cancer-detection/train_images_tif")
+    dataset_path = Path("/original_dataset/")
+    output_path = Path("/data/rsna-breast-cancer-detection/deleteme")
     convert_dataset(dataset_path, output_path)
